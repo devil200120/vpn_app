bc5f8326f6c6987c66c91976d6023075ec3c3111
Subhankar Dash <subhankar@Aganithas-MacBook-Air.local>
2026-04-03 16:39:24 +0530

    fix: use CFEnvironment.PRODUCTION for cashfree-pg v5

--- backend/config/cashfree.js
+++ backend/config/cashfree.js
@@ -1,9 +1,9 @@
-const { Cashfree } = require('cashfree-pg');
+const { Cashfree, CFEnvironment } = require('cashfree-pg');
 
 Cashfree.XClientId = process.env.CASHFREE_APP_ID;
 Cashfree.XClientSecret = process.env.CASHFREE_SECRET_KEY;
 Cashfree.XEnvironment = process.env.CASHFREE_ENV === 'PRODUCTION'
-  ? Cashfree.PRODUCTION
-  : Cashfree.SANDBOX;
+  ? CFEnvironment.PRODUCTION
+  : CFEnvironment.SANDBOX;
 
 module.exports = { Cashfree };
